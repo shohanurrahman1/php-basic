5cc0281554f9e8a6a1632736349844143d6101df
shohanurrahman1 <shohanur51361@gmail.com>
2023-03-15 23:56:15 +0600

    Form make Done

--- post.php
+++ post.php
@@ -17,21 +17,32 @@
             <!-- Form Start -->
             <form action="" method="">
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label class="form-label">First Name</label>
+                <input type="text" name="fname" class="form-control" required autocomplete="off">
               </div>
+
               <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label class="form-label">Last Name</label>
+                <input type="text" name="lname" class="form-control" required autocomplete="off">
               </div>
-              <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+              <div class="mb-3">
+                <label class="form-label">Email Address</label>
+                <input type="email" name="email" class="form-control" required autocomplete="off">
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+
+              <div class="mb-3">
+                <label class="form-label">Phone Number</label>
+                <input type="tel" name="phone" class="form-control" required autocomplete="off">
+              </div>
+
+              <div class="mb-3 d-grid gap-2">
+                <input type="submit" name="btn" class="btn btn-success" value="Submit">
+              </div>
             </form>
             <!-- Form End -->
+
+            
           </div>
         </div>
       </div>
